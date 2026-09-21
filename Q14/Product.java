84320c961d6e28c663acf341180d01f4997870a0
omkar-Riptide <omkarkinkar2007@gmail.com>
2026-09-21 08:30:14 +0530

    Add changes

---
+++ Q14/Product.java
@@ -0,0 +1,17 @@
+// Q14. Product Inventory Using Collections
+// Create a HashMap<Integer, Integer> to maintain product ID and available quantity.
+// Implement methods to:
+// •	Add products.
+// •	Update product quantity.
+// •	Search product availability.
+// •	Remove a product.
+// •	Display all products.
+// Handle the case where a requested product ID does not exist.
+public class Product {
+    public int id;public double quantity;
+
+    Product(int id,double quantity){this.id=id;this.quantity=quantity;}
+    
+
+    
+}
