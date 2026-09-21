1562bd952e12367118c7e65641fcfcb301f69a41
omkar-Riptide <omkarkinkar2007@gmail.com>
2026-09-21 22:41:37 +0530

    Added Random

--- Q12/Mainn.java
+++ Q12/Mainn.java
@@ -10,7 +10,7 @@ import  java.util.*;
 // 4.	Remove a roll number.
 // 5.	Display the final set.
 
-public class Mian {
+public class Mainn {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter no. of id's:");
@@ -39,7 +39,7 @@ public class Mian {
         }
         if(status==false){System.out.print("\nElement not found!\n");}
 
-        System.out.print("\n\nEnter roll umber to be deleted:");
+        System.out.print("\n\nEnter roll number to be deleted:");
         int del=sc.nextInt();
         rolls.remove(del);
         System.out.print(del+" removed!New set:\n");
